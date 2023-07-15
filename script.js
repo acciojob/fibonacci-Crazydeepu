15eaa354640815ec0d2f8e166ba93a24923548bb
Anudeep Reddy <95900205+Crazydeepu@users.noreply.github.com>
2023-07-15 11:30:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function fibonacci(num) {
 // your code here
+	let a = 0 , b = 1;
+	let nextTerm;
+		console.log(a +" , "+b+" , ");
+
+	for(let i=2; i<num; i++) {
+		nextTerm = a + b;
+		a = b;
+		b = nextTerm;
+		console.log(nextTerm+" , ");
+	}
 }
 
 module.exports = fibonacci;
