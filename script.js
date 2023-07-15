aa22711d5a049d9659f54100f839cf4d7ee904b8
Anudeep Reddy <95900205+Crazydeepu@users.noreply.github.com>
2023-07-15 11:57:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,15 +1,12 @@
 function fibonacci(num) {
 // your code here
-	let a = 0 , b = 1;
-	let nextTerm;
-		console.log(a +" , "+b+" , ");
+let n1 = 0, n2 = 1, nextTerm;
 
-	for(let i=2; i<num; i++) {
-		nextTerm = a + b;
-		a = b;
-		b = nextTerm;
-		console.log(nextTerm+" , ");
-	}
-}
+for (let i = 1; i <= number; i++) {
+    console.log(n1);
+    nextTerm = n1 + n2;
+    n1 = n2;
+    n2 = nextTerm;
+}	
 
 module.exports = fibonacci;
